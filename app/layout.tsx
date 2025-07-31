b7a14a40d6b48794dc2bc2893dd405df309adde8
cjsbass <cornellbasson@gmail.com>
2025-07-31 22:09:07 +0100

    Add Matrix-themed favicon and update site metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,14 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Vid Vault - Matrix YouTube Downloader',
+  description: 'Download YouTube videos with Matrix-style interface. High-quality downloads with resume capability.',
+  keywords: ['youtube', 'downloader', 'matrix', 'video', 'download'],
+  icons: {
+    icon: '/favicon.svg',
+    shortcut: '/favicon.svg',
+    apple: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({
